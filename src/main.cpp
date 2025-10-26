a68345cf4c12691cec01ec7c4acfa5a6ccb02879
perfotoran <2cakelovely@gmail.com>
2025-10-26 22:26:07 +0300

    Refactor: modularize old code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include "color.h"
+#include "vec3.h"
+
 int main(){
     int image_width = 256;
     int image_height = 256;
@@ -10,17 +13,8 @@ int main(){
     for(int j = 0; j < image_height; j++){
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for(int i = 0; i < image_width; i++){
-            double r = i / (image_width - 1.0);
-            double g = j / (image_height - 1.0);
-            double b = 0.0;
-
-            // converting from double to int format
-
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color =  color(i / (image_width - 1.0), j / (image_height - 1.0), 0);
+            write_color(std::cout, pixel_color);
         }
     }
 
