bb096c819eebb4fefd5f78b57884f1453aab4cf2
perfotoran <2cakelovely@gmail.com>
2025-10-29 20:51:58 +0300

    Add sphere with its color map

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,26 @@
 #include "ray.h"
 #include "vec3.h"
 
+double hits_sphere(point3 const& center, double radius, ray const& r){
+	vec3 oc = center - r.origin();
+	auto a = r.direction().length_squared();
+	auto h = dot(r.direction(), oc);
+	auto c = oc.length_squared() - radius * radius;
+	auto discriminant = h * h - a * c;
+	if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - std::sqrt(discriminant) ) / a;
+    }
+}
+
 color ray_color(ray const& r) {
+	auto t = hits_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0){
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
