3b4c2ce46cb6c75f9d48c47f96c4226a68675f90
perfotoran <2cakelovely@gmail.com>
2025-10-29 23:12:11 +0300

    Add hittable abstract class

---
+++ src/hittable.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "ray.h"
+
+struct hit_record{
+    point3 point;
+    vec3 normal;
+    double t;
+    bool front_face;
+
+    void set_face_normal(ray const& r, vec3 const& outward_normal){
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : - outward_normal;
+    }
+};
+
+class hittable{
+    public:
+        virtual ~hittable() = default;
+
+        virtual bool hit(ray const& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0;
+};
