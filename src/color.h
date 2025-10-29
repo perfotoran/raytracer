352f92006d30a3aaa924f8b794eba76c7f7e5a3a
perfotoran <2cakelovely@gmail.com>
2025-10-29 23:14:09 +0300

    refactor: organize code using new classes

--- src/color.h
+++ src/color.h
@@ -5,16 +5,15 @@
 using color = vec3;
 
 void write_color(std::ostream& out, color const& pixel_color) {
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
 
-    // [0, 1] - > [0, 255]
+	// [0, 1] - > [0, 255]
 
-    int rbyte = static_cast<int>(255.999 * r);
-    int gbyte = static_cast<int>(255.999 * g);
-    int bbyte = static_cast<int>(255.999 * b);
+	int rbyte = static_cast<int>(255.999 * r);
+	int gbyte = static_cast<int>(255.999 * g);
+	int bbyte = static_cast<int>(255.999 * b);
 
-
-    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+	out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,35 @@
-#include <iostream>
 
-#include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable.h"
+#include "hittable_list.h"
+#include "rtweekend.h"
+#include "sphere.h"
 
-double hits_sphere(point3 const& center, double radius, ray const& r){
-	vec3 oc = center - r.origin();
-	auto a = r.direction().length_squared();
-	auto h = dot(r.direction(), oc);
-	auto c = oc.length_squared() - radius * radius;
-	auto discriminant = h * h - a * c;
-	if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (h - std::sqrt(discriminant) ) / a;
-    }
-}
-
-color ray_color(ray const& r) {
-	auto t = hits_sphere(point3(0, 0, -1), 0.5, r);
-	if (t > 0.0){
-		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(ray const& r, hittable_list const& world) {
+	hit_record rec;
+	if(world.hit(r, 0, infinity, rec)){
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+	vec3 unit_direction = unit_vector(r.direction());
+	auto a = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
 	// Image
 
 	auto aspect_ratio = 16.0 / 9.0;
-	int image_width = 400;
+	int image_width = 1920;
 
 	int image_height = static_cast<int>(image_width / aspect_ratio);
 	image_height = (image_height < 1) ? 1 : image_height;
+
+	// World
+
+	hittable_list world;
+
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	// Camera
 
@@ -54,7 +47,7 @@ int main() {
 	auto viewport_upper_left =
 	    camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
 
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 	// Render (output PPM format image)
 
@@ -62,11 +55,11 @@ int main() {
 	for (int j = 0; j < image_height; j++) {
 		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++) {
-            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-            
-            color pixel_color = ray_color(r);
+			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_direction = pixel_center - camera_center;
+			ray r(camera_center, ray_direction);
+
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
