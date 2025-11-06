cc4d36ba4261c417e800337c2dc1447b24a7cf66
perfotoran <2cakelovely@gmail.com>
2025-11-07 00:19:56 +0300

    Add fuzziness to metal materials

--- src/material.h
+++ src/material.h
@@ -37,15 +37,17 @@ public:
 class metal : public material {
 private:
 	color albedo;
+	double fuzz;
 
 public:
-	metal(color const& albedo) : albedo(albedo) {}
+	metal(color const& albedo, double fuzz) : albedo(albedo), fuzz(fuzz) {}
 
 	bool scatter(ray const& r_in, hit_record const& rec, color& attenuation,
 	             ray& scattered) const override {
 		vec3 reflected = reflect(r_in.direction(), rec.normal);
+		reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
 		scattered = ray(rec.point, reflected);
 		attenuation = albedo;
-		return true;
+		return dot(scattered.direction(), rec.normal) > 0;
 	}
 };
