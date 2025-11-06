8e1ad7fb9d86219df4e828c9a6e80d4881af74bc
perfotoran <2cakelovely@gmail.com>
2025-11-07 02:01:14 +0300

    Add dielectric material

--- src/material.h
+++ src/material.h
@@ -51,3 +51,33 @@ public:
 		return dot(scattered.direction(), rec.normal) > 0;
 	}
 };
+
+class dielectric : public material {
+private:
+	double refraction_index;
+
+public:
+	dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+	bool scatter(ray const& r_in, hit_record const& rec, color& attenuation,
+	             ray& scattered) const override {
+		attenuation = color(1.0, 1.0, 1.0);
+		double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+		double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+		double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+		bool cannot_refract = ri * sin_theta > 1.0;
+		vec3 direction;
+
+		if (cannot_refract)
+			direction = reflect(unit_direction, rec.normal);
+		else
+			direction = refract(unit_direction, rec.normal, ri);
+
+		scattered = ray(rec.point, direction);
+
+		return true;
+	}
+};
--- src/vec3.h
+++ src/vec3.h
@@ -105,3 +105,10 @@ inline vec3 random_on_hemisphere(vec3 const& normal) {
 }
 
 inline vec3 reflect(vec3 const& v, vec3 const& n) { return v - 2 * dot(v, n) * n; }
+
+inline vec3 refract(vec3 const& uv, vec3 const& n, double etai_over_etat) {
+	auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+	vec3 r_out_perp = etai_over_etat * (uv + cos_theta * n);
+	vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+	return r_out_perp + r_out_parallel;
+}
